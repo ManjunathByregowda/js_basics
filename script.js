9a9a69a4c3457e0723b17750c2145e090a727c68
Manju Gowda <manju.gowda@saranyu.in>
2021-05-23 10:59:43 +0530

    js practise

--- script.js
+++ script.js
@@ -1,8 +1,43 @@
 console.log("Loading JS")
 
 
-var a = "3"
-var b = 3
-var c
-var d = null
-var e = true
+var a = "3";
+console.log(a, typeof(a));
+
+var b = 3;
+console.log(b, typeof(b));
+
+var c;
+console.log(c, typeof(c));
+
+var d = null;
+console.log(d, typeof(d));
+
+var e = true;
+console.log(e, typeof(e));
+
+var f = Symbol()
+console.log(f, typeof(f));
+
+
+
+// Hosting
+console.log(g)
+var g;
+
+// if
+a = 3, b = 2
+
+if (a > b) {
+    console.log(a,"is greater")
+} else {
+    console.log(b,"is greater")
+}
+
+// Function
+
+function add(a, b) {
+   var sum = a + b;
+   console.log("Total count is", sum)
+}
+add(55, 33);
